b24bed9992765c25df8c55d5dcb2b17f6f0aa1e3
AvranWong <66788863+AvranWong@users.noreply.github.com>
2021-11-05 21:49:49 +0800

    Update Reservation_UI.java

--- src/Reservation/Reservation_UI.java
+++ src/Reservation/Reservation_UI.java
@@ -3,46 +3,55 @@ package Reservation;
 import java.util.Scanner;
 
 public class Reservation_UI {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        int choice;
-        do {
-            System.out.println("(1) Create Reservations");
-            System.out.println("(2) View Reservations");
-            System.out.println("(3) Delete Reservation");
-            System.out.println("(4) Exit");
-            choice = sc.nextInt();
-            switch (choice) {
-            case 1: // Create Reservations
-                System.out.println("Please enter Reservation start time");
-                int startDateTime = sc.nextInt();
-                System.out.println("Please enter Reservation end time: ");
-                int endDateTime = sc.nextInt();
-                System.out.println("Please enter the pax: ");
-                int pax = sc.nextInt();
-                System.out.println("Please enter your name: ");
-                String name = sc.next();
-                System.out.println("Please enter your contact: ");
-                int contact = sc.nextInt();
-                Reservation_Control.createReservation(startDateTime, endDateTime, pax, name, contact);
-                break;
-            case 2: // View Reservations
-                System.out.println("Please enter your name");
-                String x = sc.next();
-                System.out.println("Please enter your contact number");
-                int y = sc.nextInt();
-                Reservation_Control.viewReservation(x, y);
-                break;
-            case 3: // Delete Reservation
-                System.out.println("Please enter your name");
-                String z = sc.next();
-                Reservation_Control.deleteReservation(z);
-                break;
-            case 4: // Exit
-                System.out.println("Exiting...");
-                break;
-            }
-        } while (choice < 4);
-    }
+	
+	public static void showReservationUI() {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		do {
+			System.out.println("(1) Create Reservations");
+			System.out.println("(2) View Reservations");
+			System.out.println("(3) Delete Reservation");
+			System.out.println("(4) Return to main menu");
+			choice = sc.nextInt();
+			switch (choice) {
+				case 1: // Create Reservations
+					System.out.println("Please enter Reservation date (dd/mm/yy)");
+					//String date = sc.next();
+					// Only can input 1000 1200 1400 1600 1800
+					System.out.println("Please enter Reservation Time (hh:mm) ");
+					//String time = sc.next();
+					System.out.println("Please enter the pax: ");
+					//int pax = sc.nextInt();
+					System.out.println("Please enter your name: ");
+					//String name = sc.next();
+					System.out.println("Please enter your contact: ");
+					//int contact = sc.nextInt();
+					
+					//TEST
+					String time = "10:00";
+					String date = "05/11/21";
+					int pax = 5;
+					String name = "avran";
+					int contact = 1234;
+					Reservation_Control.createReservation(date , time, pax, name, contact);
+					break;
+				case 2: // View Reservations
+					System.out.println("Please enter your name");
+					String x = sc.next();
+					System.out.println("Please enter your contact number");
+					int y = sc.nextInt();
+					Reservation_Control.viewReservation(x,y);
+					break;
+				case 3: // Delete Reservation
+					System.out.println("Please enter your name");
+					String z = sc.next();
+					Reservation_Control.deleteReservation(z);
+					break;
+				case 4: // Exit
+					System.out.println("Returning to main menu...");
+					break;
+				}
+			} while (choice < 4);
+		}
 }
