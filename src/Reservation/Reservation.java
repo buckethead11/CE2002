63a3dd6833b07c764ffb4d9d363344645eaa996e
AvranWong <66788863+AvranWong@users.noreply.github.com>
2021-11-08 00:21:21 +0800

    Update Reservation.java

--- src/Reservation/Reservation.java
+++ src/Reservation/Reservation.java
@@ -1,41 +1,49 @@
-package Reservation;
+package Reservations;
 
 import java.util.Calendar;
 
 public class Reservation {
 	private Calendar dateTime;
+	private int pax;
 	private String name;
 	private int contact;
-	private boolean occupied;
+	private int tableID;
+	
 	// Constructor
-	public Reservation( Calendar dateTime) {
+	public Reservation( Calendar dateTime, int pax, String name, int contact, int tableID) {
 		//Constructs reservation with a variable start and end time. pax name and contact is defaulted as 0/NA
 		this.dateTime = dateTime;
-		name = "NA";
-		contact = 0;
-		occupied = false;
+		this.pax = pax;
+		this.name = name;
+		this.contact = contact;
+		this.tableID = tableID;
 		}
 	
 	//Getters
 	public Calendar getDateTime() {
-		
-		return dateTime;
+		return this.dateTime;
 	}
-	
-	
+	public int getPax() {
+		return this.pax;
+	}
 	public String getName() {
 		return this.name;
 	}
 	public int getContact() {
 		return this.contact;
 	}
-	public boolean getOccupied() {
-		return this.occupied;
+	
+	public int getTableID() {
+		return this.tableID;
 	}
 	
+	
 	//Setters
-	public void setOccupied(boolean bool) {
-		this.occupied = bool;
+	public void setDateTime(Calendar dateTime) {
+		this.dateTime = dateTime;
+	}
+	public void setPax(int pax) {
+		this.pax = pax;
 	}
 	public void setName(String name) {
 		this.name = name;
@@ -43,4 +51,9 @@ public class Reservation {
 	public void setContact(int contact) {
 		this.contact = contact;
 	}
+	public void getTableID(int tableID) {
+		this.tableID = tableID;
+	}
+	
+	
 }
