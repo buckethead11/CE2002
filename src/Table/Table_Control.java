79cb7472e6c21a8170eb5862d6c0ae4d74e373a8
AvranWong <66788863+AvranWong@users.noreply.github.com>
2021-11-08 00:20:39 +0800

    Refactored table_control
    simplified table control to only managing tables

--- src/Table/Table_Control.java
+++ src/Table/Table_Control.java
@@ -16,8 +16,6 @@ public class Table_Control {
 	public static String pathSeparator = File.separator; 
 	private static String pathToCsv = userHome+ pathSeparator + "src" + pathSeparator + "data"+ pathSeparator + "tables.csv";
 	
-	
-	
 	//Initialises the table layout
 	public static void init() {
 		//create an array list of table layout with 20 tables
@@ -47,39 +45,9 @@ public class Table_Control {
 		
 	}
 	
-	
-	//Methods
-	public static int checkTable(Calendar dateTime, int pax) {
-		int i = 0;
-		int index = 0;
-		//Iterates through the table layout
-		for (i =0;i<10;i++) {
-			//If table can fit the pax and is not occupied
-			if (tableLayout.get(i).getPax() >= pax && tableLayout.get(i).getReservation(i).getOccupied() == false ){
-				System.out.println("Table found!");
-				index = i;
-				return index;
-				}else{
-					System.out.println ("There are no tables avaliable");
-					return 0;
-				}
-			}
-		//returns the table id to the reservation controller
-		return tableLayout.get(i).getTableID();
-		}	
-	//assigns the reservation to the table
-	public static void assignTable(Calendar dateTime, int pax, String name, int contact,int tableID) {
-		//Pushes the reservation to the table
-		//Gets the index of the reservation array based on the timing
-		int index = tableLayout.get(tableID).getReservationIndex(dateTime);
-		//Sets the name of the reservation
-		tableLayout.get(tableID).getReservation(index).setName(name);
-		//Sets the contact of the reservation
-		tableLayout.get(tableID).getReservation(index).setContact(contact);
-		//Sets occupied to true
-		tableLayout.get(tableID).getReservation(index).setOccupied(true);
-		System.out.println("Table assigned!");
-		
+	public static ArrayList<Table> getTableLayout(){
+		return tableLayout;
 	}
+
 }
 
