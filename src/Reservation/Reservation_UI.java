db71845f136d1fe73391668c3b53a2ea1849f7ac
AvranWong <66788863+AvranWong@users.noreply.github.com>
2021-11-08 21:42:31 +0800

    Added saveReservation

--- src/Reservation/Reservation_UI.java
+++ src/Reservation/Reservation_UI.java
@@ -1,4 +1,4 @@
-package Reservation;
+package reservations;
 
 import java.util.Scanner;
 
@@ -17,23 +17,16 @@ public class Reservation_UI {
 			switch (choice) {
 				case 1: // Create Reservations
 					System.out.println("Please enter Reservation date (dd/mm/yy)");
-					//String date = sc.next();
-					// Only can input 1000 1200 1400 1600 1800
+					String date = sc.next();
 					System.out.println("Please enter Reservation Time (hh:mm) ");
-					//String time = sc.next();
+					String time = sc.next();
 					System.out.println("Please enter the pax: ");
-					//int pax = sc.nextInt();
+					int pax = sc.nextInt();
 					System.out.println("Please enter your name: ");
-					//String name = sc.next();
+					String name = sc.next();
 					System.out.println("Please enter your contact: ");
-					//int contact = sc.nextInt();
+					int contact = sc.nextInt();
 					
-					//TEST
-					String time = "10:00";
-					String date = "05/11/21";
-					int pax = 5;
-					String name = "avran";
-					int contact = 1234;
 					Reservation_Control.createReservation(date , time, pax, name, contact);
 					break;
 				case 2: // View Reservations
@@ -49,6 +42,7 @@ public class Reservation_UI {
 					Reservation_Control.deleteReservation(z);
 					break;
 				case 4: // Exit
+					Reservation_Control.saveReservation();
 					System.out.println("Returning to main menu...");
 					break;
 				}
