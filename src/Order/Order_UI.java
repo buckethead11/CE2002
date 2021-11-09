e558439c93a497ac3d224fcdaf69653d4738fb35
christoarif <casetiadharma@gmail.com>
2021-11-09 12:32:37 +0800

    clean up order_UI

--- src/Order/Order_UI.java
+++ src/Order/Order_UI.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import Menu.*;
 
 public class Order_UI {
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         Menu_Control.loadMenuItem();
@@ -11,7 +12,6 @@ public class Order_UI {
     }
 
     public static void showOrderUI() {
-        Scanner sc = new Scanner(System.in);
         int input;
         do {
             System.out.println("(1) View a order");
@@ -25,68 +25,33 @@ public class Order_UI {
 
             switch (input) {
             case 1: // view an order
-                System.out.println("Please enter the tableID to view order");
-                // try {
-                input = sc.nextInt();
-                orderManager.printOrderedItems(input);
+                ViewOrder();
                 break;
-
             // } catch (Exception e) {
             // System.out.println("Error: Please enter a valid input");
             // }
 
             case 2: // create a new Order
                 // try {
-                System.out.println("Enter Table ID");
-                int tableID = sc.nextInt();
-                System.out.println("Enter Staff ID");
-                int staffID = sc.nextInt();
-                System.out.println("Membership (1/0)");
-                boolean membership = sc.nextBoolean();
-                orderManager.makeOrder(tableID, membership, staffID);
+                createNewOrder();
                 // } catch (Exception e) {
                 // System.out.println("Please enter an integer for Order Number or Staff ID");
                 // }
                 break;
 
             case 3:// add an item into order
-                System.out.println("Pleaes key in the tableID you want to add order");
-                int tableID2 = sc.nextInt();
-                System.out.println("Please enter itemID to be added");
-                int itemID = sc.nextInt();
-                System.out.println("Please enter Quantity");
-                int quanitity = sc.nextInt();
-                orderManager.addItemToOrder(tableID2, itemID, quanitity);
-                /*
-                 * try{
-                 * 
-                 * 
-                 * } catch (Exception e){
-                 * System.out.println("Error: Please enter a valid value"); }
-                 */
+                addItem();
                 break;
 
-            case 4:// delete an item from other
-                System.out.println("Please key in the tableID you want to remove order");
-                // try {
-                int tableID3 = sc.nextInt();
-                System.out.println("Please enter itemID to be deleted ");
-                int itemID2 = sc.nextInt();
-                System.out.println("Please enter Quantity");
-                int quantity = sc.nextInt();
-                orderManager.removeItemFromOrder(tableID3, itemID2, quantity);
+            case 4:// delete an item from order
+                deleteItem();
 
-                // } catch (Exception e) {
-                // System.out.println("Error: Please enter a valid value");
-                // }
                 break;
 
             case 5: // Delete an order
                 // System.out.println("Printing out all the orders");
                 // orderManager.getAllOrders();
-                System.out.println("Which tableID order do you want to delete?");
-                tableID = sc.nextInt();
-                orderManager.removeOrder(tableID);
+                deleteOrder();
                 break;
             case 6:
                 System.out.println("Enter new service charge rate percentage (whole numbers only)");
@@ -102,4 +67,51 @@ public class Order_UI {
         } while (true);
     }
 
+    public static void ViewOrder() {
+        System.out.println("Please enter the tableID to view order");
+        int input = sc.nextInt();
+        orderManager.printOrderedItems(input);
+    }
+
+    public static void createNewOrder() {
+        System.out.println("Enter Table ID");
+        int tableID = sc.nextInt();
+        System.out.println("Enter Staff ID");
+        int staffID = sc.nextInt();
+        System.out.println("Membership (TRUE/FALSE)");
+        boolean membership = sc.nextBoolean();
+        orderManager.makeOrder(tableID, membership, staffID);
+
+    }
+
+    public static void deleteOrder() {
+        System.out.println("Which tableID order do you want to delete?");
+        int tableID = sc.nextInt();
+        orderManager.removeOrder(tableID);
+    }
+
+    public static void addItem() {
+        System.out.println("Pleaes key in the tableID you want to add order");
+        int tableID2 = sc.nextInt();
+        System.out.println("Please enter itemID to be added");
+        int itemID = sc.nextInt();
+        System.out.println("Please enter Quantity");
+        int quanitity = sc.nextInt();
+        orderManager.addItemToOrder(tableID2, itemID, quanitity);
+    }
+
+    public static void deleteItem() {
+        System.out.println("Please key in the tableID you want to remove order");
+        // try {
+        int tableID3 = sc.nextInt();
+        System.out.println("Please enter itemID to be deleted ");
+        int itemID2 = sc.nextInt();
+        System.out.println("Please enter Quantity");
+        int quantity = sc.nextInt();
+        orderManager.removeItemFromOrder(tableID3, itemID2, quantity);
+
+        // } catch (Exception e) {
+        // System.out.println("Error: Please enter a valid value");
+        // }
+    }
 }
