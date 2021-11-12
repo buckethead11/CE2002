4a746d23cb208cfbc52acb7c055ab82011b5779f
Hui Yao <leehuiyao@outlook.com>
2021-11-12 14:03:27 +0800

    Can finally print sales report

--- src/Sales/Sales_UI.java
+++ src/Sales/Sales_UI.java
@@ -3,13 +3,16 @@ package Sales;
 import java.util.*;
 
 import Menu.Menu_Control;
+import Menu.PromotionPackage;
 import Order.*;
 public class Sales_UI {
     
     private static HashMap<Integer,Order> totalOrders = new HashMap<Integer,Order>();
     private static HashMap<Integer, Integer> individualOrder ;//= new HashMap<Integer, Integer>();
     private static HashMap<Integer, Integer> individualPackage ;//= new HashMap<Integer, Integer>();
-    private static HashMap<Integer, Integer> totalQty ;
+    private static HashMap<Integer, Integer> totalItemQty = new HashMap<Integer, Integer>();
+    private static HashMap<Integer, Integer> totalPackageQty = new HashMap<Integer, Integer>();
+
     public static void main(String[] args) {
         start();
     }
@@ -21,39 +24,80 @@ public class Sales_UI {
     public static void start(){
 
         totalOrders = Order_Control.getOrderList();
+
+        
         totalOrders.forEach((key,order) ->{
             totalSales += order.getTotalPrice();
             individualOrder = order.getOrderedItems();
             individualPackage = order.getOrderedPackages();
 
-            individualOrder.forEach((itemID,qty)->{
-                oldQty = totalQty.getOrDefault(itemID,0);
-                totalQty.put(itemID,oldQty +qty);
-                
-            });
-            individualPackage.forEach((itemID,qty)->{
-                oldQty = totalQty.getOrDefault(itemID,0);
-                totalQty.put(itemID,oldQty +qty);
-            });
-        });
+            if (!individualOrder.isEmpty()){
 
-       // printSales();
+                individualOrder.forEach((itemID,qty)->{
+                    System.out.println("ItemID:" +itemID + "Qty :"+qty);
+                    oldQty = totalItemQty.getOrDefault(itemID,0);
+    
+                    if (oldQty == 0){ //not in the list
+                        totalItemQty.put(itemID, qty);
+                    }
+                    else{
+                        totalItemQty.replace(itemID, oldQty+qty);
+                    }
+                });
+            }
+
+            
+            if(!individualPackage.isEmpty()){
+                individualPackage.forEach((packageID,packageQty)->{
+                    System.out.println("Package:" +packageID + "Qty :"+packageQty);
+                    oldQty = totalPackageQty.getOrDefault(packageID,0);
+    
+                    if (oldQty == 0){ //not in the list
+                        totalPackageQty.put(packageID, packageQty);
+                    }
+                    else{
+                        totalPackageQty.replace(packageID, oldQty+packageQty);
+                    }
+                });
+            }
+               
+    });
+
+        printSales();
 
         System.out.println("Total sale: "+ totalSales);
+        
     }
             
-    /*
+
     public static void printSales(){
-        
-        totalQty.forEach((itemID,qty)->{
+        System.out.println("--------------------Individual Sales Report---------------------");
+        System.out.println("Item\t\t   |Qty\t\t |Total Price");
+        totalItemQty.forEach((itemID,qty)->{
+            if (qty!=0){
                 System.out.println(
-                    "Item: " +Menu_Control.getMenuItem(itemID)
-                    +" Qty: "+ qty
+                    Menu_Control.getMenuItem(itemID+1).getName()
+                    +"\t\t"+ qty + (Menu_Control.getMenuItem(itemID+1).getPrice()*qty)
+                    
                 );
             }
-        );
+        });
+
+//This does not work because the promotion package class contains the description and not the itemID
+    System.out.println("--------------------Package Sales Report---------------------");
+    System.out.println("Package\t\t   |qty\t\t |Price");
+        totalPackageQty.forEach((packageID,qty)->{
+            if (qty!=0){
+                packageID+=1;
+                System.out.println(
+                    packageID
+                    +"\t\t"+ qty +"\t\t" + (Menu_Control.getPromoPackageList().get(packageID).getPackagePrice())*qty
+                );
+            }
+        });
+        
     }
-    */
+
 
     public static void displayUI(){
 
